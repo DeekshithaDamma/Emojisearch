c79c85b21c4816307cba918429d823c3a13e8dea
DeekshithaDamma <135822283+DeekshithaDamma@users.noreply.github.com>
2023-08-02 12:18:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import './App.css'
+// import ClickCounter from './Components/ClickCounter';
+// import HoverCount from './Components/HoverCount';
+// import UpdatedComponents from './Components/WithCounter';
+//import Navbar from './Components/Navbar';
+import EmojiSearch from './Components/EmojiSearch';
+//import NameList from './Components/NameList';
+// import FunctionClick from './Components/FunctionClick';
+// import ClassClick from './Components/ClassClick';
+//import NameList from './Components/NameList';
+//import EmojiPicker from 'emoji-picker-react';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <FunctionClick/> */}
+      {/* <ClassClick/> */}
+      <EmojiSearch/>
+       {/* <EmojiPicker/> */}
+       {/* <Navbar/> */}
+       {/* <ClickCounter/>
+       <HoverCount/>
+       <UpdatedComponents/> */}
+    </div>
+  );
+}
+
+export default App;
